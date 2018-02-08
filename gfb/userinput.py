aa6a6d36434101f14cae8e81b24ac3e2226b986e
Norm Barnard <barnardn@gmail.com>
2018-02-07 21:34:39 -0500

    moved line input to method better handling of keyboard interrupt

--- gfb/userinput.py
+++ gfb/userinput.py
@@ -1,6 +1,7 @@
 import sys
 import readchar
 import typing
+import getpass
 
 def read_response(prompt: str=None) -> str:
     """
@@ -15,3 +16,23 @@ def read_response(prompt: str=None) -> str:
     ch = readchar.readchar()
     print("{0}\n".format(ch))
     return ch
+
+def prompt(prompt: str, secure: bool=False):
+    """
+    Reads a line of input from the user
+    params:
+        prompt: input prompt
+        secure: read with getpass
+    returns:
+        the string entered
+    """
+    result = ""
+    try:
+        if secure:
+            result = getpass.getpass(prompt)
+        else:
+            result = input(prompt)
+    except KeyboardInterrupt:
+        sys.exit(1)
+    else:
+        return result
